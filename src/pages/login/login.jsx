519d00c862420ac990148e2b3236cae7d27c72fe
joeljosh <joeljoshua0704@gmail.com>
2022-07-28 20:46:48 +0530

    Update login.jsx

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -1,5 +1,6 @@
-import React,{useState,useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 
+import { useNavigate } from "react-router-dom";
 import Button from "@mui/material/Button";
 import { styled } from '@mui/material/styles';
 import logo from "../../assets/icons/logo.png";
@@ -12,32 +13,38 @@ import './login.css'
 
 
 const ColorButton = styled(Button)(({ theme }) => ({
-    margin: theme.spacing(1),
-    borderRadius: 25,
-    width: 260,
-    color: "#fff",
-    color: theme.palette.getContrastText("#fff"),
-    backgroundImage:
-      "linear-gradient( 136deg, rgb(242,113,33) 0%, rgb(233,64,87) 50%, rgb(138,35,135) 100%)",
+  margin: theme.spacing(1),
+  borderRadius: 25,
+  width: 260,
+  color: "#fff",
+  color: theme.palette.getContrastText("#fff"),
+  backgroundImage:
+    "linear-gradient( 136deg, rgb(242,113,33) 0%, rgb(233,64,87) 50%, rgb(138,35,135) 100%)",
 }));
 
 
 export const Login = () => {
-    const [show, setShow] = useState(false);
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
+  let navigate = useNavigate();
+  const [show, setShow] = useState(false);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-    function Togglelogin() {
-      if (!show) {
-        setShow(true);
-        return;
-      } else {
-        // handleLogin();
-      }
+
+  function handlelogin() {
+    navigate("/home");
+  }
+
+  function Togglelogin() {
+    if (!show) {
+      setShow(true);
+      return;
+    } else {
+      handlelogin();
     }
+  }
 
-    return(
-        <div className="login">
+  return (
+    <div className="login">
       <div className="welcome">
         <h1>Welcome,</h1>
         <h3>lets get started.</h3>
@@ -93,7 +100,7 @@ export const Login = () => {
         <br></br>
         <div style={{ color: "gray", padding: "12px 8px 4px" }}>Or</div>
         <h3 className="gradient">
-        Create an account?
+          Create an account?
           {/* <Link to="/signup">Create an account?</Link> */}
         </h3>
       </div>
@@ -106,5 +113,5 @@ export const Login = () => {
         }}
       /> */}
     </div>
-    )
+  )
 }
